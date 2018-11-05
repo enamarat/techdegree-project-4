517c1d0abc0f2129fb40e80d653d8ab02b08da67
Marat Enaliev <enamarat@yandex.ru>
2018-11-05 20:38:11 +0300

    Comments are added
    Descriptive comments now supplement the code.

--- js/game.js
+++ js/game.js
@@ -21,27 +21,28 @@ class Game {
   }
 
 
-  /* A function which generates random quotes*/
+  /* A function which generates random quotes. */
      getRandomPhrase() {
       let randomPhrase = Math.floor(Math.random() * this.phrases.length);
       return this.phrases[randomPhrase];
     }
 
-/* This function picks a random phrase and adds it to the screen*/
+  /* This function picks a random phrase. */
     generatedPhrase() {
       return new Phrase(this.getRandomPhrase());
     }
 
 
-// Good!
+    /* This function adds a chosen random phrase to te screen. */
     startGame() {
       this.addedPhrase.addPhraseToDisplay();
       console.log(this.addedPhrase);
     }
 
-    /* This method displays a message which varies depending on whether player has won or lost */
+    /* This method ends the game by disabling all remaining buttons which weren`t pressed,
+     and it also displays a message which varies depending on whether a player has won or lost. */
         gameOver() {
-          // If all letters in the phrase are guessed
+          // If all letters in the phrase are guessed, players will see a message about their victory.
           if (this.missed < 5) {
             const messageOfVictory = document.createElement('span');
             messageOfVictory.textContent = "Victory and glory are yours!";
@@ -50,58 +51,54 @@ class Game {
             document.querySelector("#scoreboard").appendChild(messageOfVictory);
           }
 
-          // If player missed 5 times, he lost, and corresponding message will appear
+          // If players missed 5 times, they lost, and corresponding message will appear.
           if (this.missed === 5) {
             const messageOfDefeat = document.createElement('span');
             messageOfDefeat.textContent = "You have failed miserably!";
             messageOfDefeat.style.fontWeight = "bold";
             messageOfDefeat.style.color = "red";
             document.querySelector("#scoreboard").appendChild(messageOfDefeat);
-
           }
-
           const keys = document.querySelectorAll(".key");
           for (let i = 0; i < keys.length; i++) {
             keys[i].disabled = true;
           }
         }
 
-/* This method removes a heart from the board, and,
- if the player is out of lives, ends the game */
-    removeLife() {
-      this.missed += 1;
-        if (this.missed < 6) {
-            document.querySelectorAll(".tries")[this.missed - 1].style.display = "none";
+    /* This method removes a heart from the board, and,
+     if the player is out of lives, ends the game */
+      removeLife() {
+        this.missed += 1;
+          if (this.missed < 6) {
+              document.querySelectorAll(".tries")[this.missed - 1].style.display = "none";
+          }
+          // If 5 lives are lost, the game is over
+          if (this.missed === 5) {
+            this.gameOver();
+          }
         }
-        // If 5 lives are lost, the game is over
-        if (this.missed === 5) {
-          this.gameOver();
+
+        /* This method checks to see if the player has selected all of the letters
+        in the phrase */
+        checkForWin() {
+        let characterToDisplay = document.getElementsByClassName("letter");
+        let arrayOfLetters = Array.from(characterToDisplay);
+          if (this.guessed === arrayOfLetters.length) {
+            document.querySelector('#scoreboard ol').style.display = "none";
+            this.gameOver();
+          }
+      }
+
+      /* When any button is pressed this function checks whether the chosen
+      letter is present in the generated phrase. If it's not, a life is removed. If it is,
+      the letter is displayed on the screen and whether a player has met all the requirements for
+      victory is checked. */
+      handleInteraction() {
+        if (this.addedPhrase.checkLetter().length > 0) {
+          this.addedPhrase.showMatchedLetter();
+          this.checkForWin();
+        } else {
+          this.removeLife();
         }
       }
-
-// This method checks to see if the player has selected all of the letters
-    checkForWin() {
-    //console.log(this.guessed);
-    let characterToDisplay = document.getElementsByClassName("letter");
-    let arrayOfLetters = Array.from(characterToDisplay);
-    //console.log(arrayOfLetters.length);
-      if (this.guessed === arrayOfLetters.length) {
-        document.querySelector('#scoreboard ol').style.display = "none";
-        this.gameOver();
-      }
-
-    }
-
-
-
-    handleInteraction() {
-      if (this.addedPhrase.checkLetter().length > 0) {
-
-        this.addedPhrase.showMatchedLetter();
-        this.checkForWin();
-      } else {
-        this.removeLife();
-      }
-    }
-
 }
--- js/phrase.js
+++ js/phrase.js
@@ -3,14 +3,11 @@ class Phrase {
     this.phrase = phrase;
   }
 
-
-/*Function which adds letter placeholders to the display
-  when the game starts. When the player correctly guesses a
-  letter, the empty box is replaced with a the matched
-  letter */
+  /* Function which adds letter placeholders to the display
+    when the game starts. The number of placeholders is determined by
+    generated random phrase. */
     addPhraseToDisplay() {
       const ul =  document.querySelector('#phrase');
-
       for (let character in this.phrase) {
           const li = document.createElement('li');
           let letterToDisplay = ul.appendChild(li);
@@ -23,20 +20,21 @@ class Phrase {
       }
     }
 
-/*Function which checks to see if letter selected by
-  player matches a letter in the phrase*/
+  /* Function which checks to see if letter selected by player matches
+   a letter in the phrase. */
     checkLetter() {
     let matched = this.phrase.split('').filter(letter => letter.toLowerCase() === event.target.textContent);
     return matched;
     }
 
 
-/*Function which reveals the letter(s) on the board that
- matches player's selection*/
+  /* Function which reveals the letter(s) on the board that
+   matches player's selection. */
     showMatchedLetter() {
       let characterToDisplay = document.getElementsByClassName("letter");
       let arrayOfLetters = Array.from(characterToDisplay);
-
+      /* Looping through letters of the generated phrase to see
+      whether any of them matches player's selection*/
       for (let i = 0; i < arrayOfLetters.length; i++) {
         if (arrayOfLetters[i].textContent.toLowerCase() === event.target.textContent) {
           arrayOfLetters[i].style.backgroundColor = "MediumSpringGreen";
@@ -44,10 +42,7 @@ class Phrase {
           arrayOfLetters[i].style.color = "black";
           event.target.style.backgroundColor = "lime";
           game.guessed += 1;
-
         }
       }
-
-      }
-
     }
+  }
